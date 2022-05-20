75f3f79d63310a5d0f89557488bfa8493c90470f
asluchych <anatolblr@gmail.com>
2022-05-20 17:40:36 +0200

    Update chapter2.R

--- chapter2.R
+++ chapter2.R
@@ -37,53 +37,52 @@ priceScaled_200 <- predict(linModC, data.frame(SQM = c(200)))
 # The prediction is still in 1000$. For interpretation, calculate in $ * 1000:
 priceInDollar_200 <- priceScaled_200 * 1000 
 
-##### Aufgabe 5: Schätzunsicherheit #####
+#####  Assignment 5: Estimation Uncertainty #####
 
-# Daten einlesen
+# Read data
 random <- read.csv("random.csv")
 
-# a) Lineares Modell y1 ~ x
+# a) linear model y1 ~ x
 regA <- lm(Y1 ~ X, data = random)
 summary(regA)
-# (i) Kovarianz Matrix der Koeffizienten
+# (i) Covariance matrix of the coefficients
 vcov(regA)
-# (ii) Ohne Konstante/Intercept - In R wird die Konstante standardmäßig in die
-# Regression mit aufgenommen, das verhindert man, indem man -1 in die Gleichung
-# aufnimmt
-regOhneK <- lm(Y1 ~ -1 + X, data = random)
-summary(regOhneK)
+# (ii) Without a constant/intercept - By default, intercept is included in 
+# a regression in R. To estimate the model without an intercept, include -1 in 
+# the equation
+regNoInt <- lm(Y1 ~ -1 + X, data = random)
+summary(regNoInt)
 
-# b) Lineares Modell y2 ~ x
+# b) Linear model y2 ~ x
 regB <- lm(Y2 ~ X, data = random)
 summary(regB)
 
-# zwei Scatterplotts in einem Plot
+# Two scatterplots in one plot
 plot(Y1 ~ X, data = random, col = "blue",  xlab='X', ylab='Y1 and Y2',
-     xlim=c(14,47), ylim=c(25,100), main='Scatterplot') # Plot 1
-par(new = TRUE) # Plotte den nächsten Plot in dem davor
+     xlim=c(14,47), ylim=c(25,100), main='Scatterplot') # plot 1
+par(new = TRUE) # plot the next plot in the previous one
 plot(Y2 ~ X, data = random, col = "red", xlab='X', ylab='Y1 and Y2',
-     xlim=c(14,47), ylim=c(25,100)) # Plot 2
-abline(a = 1, b = 2, col = "green") # Gerade mit wahrem Zusammenhang
+     xlim=c(14,47), ylim=c(25,100)) # plot 2
+abline(a = 1, b = 2, col = "green") # true model line
 legend("bottomright", legend=c('Y1', 'Y2', 'True Model'),
        col=c('blue', 'red', 'green'), pch = c(1, 1, NA_integer_),
-       lty = c(0, 0, 1)) # Legende hinzufügen
+       lty = c(0, 0, 1)) # add a legend
 
 
-# c) Verringere Variation von X
-# (i) durch Skalierung von X
+# c) Reduce the variation of x 
+# (i) by rescaling of X
 random$xScaled <- random$X/10
-# Modell aus b) neu schätzen
+# Re-estimate the model from b)
 regScaledX <- lm(Y2 ~ xScaled, data = random)
 summary(regScaledX)
-# (ii) durch veringerung de Stichprobengröße, nutzt nur die ersten 100 
-# Beobachtungen für die Schätzung
+# (ii) by reducing the sample size, use the first 100 observations
 regSmallSample <- lm(Y2 ~ X, data = random[1:100,])
 summary(regSmallSample)
 
-# d) Umkehrregressionen
-# (1) zur Regression in a)
-regUmkehrA <- lm(X ~ Y1, data = random)
-summary(regUmkehrA)
-# (2) zur Regression in b)
-regUmkehrB <- lm(X ~ Y2, data = random)
-summary(regUmkehrB)
+# d) inverse regression
+# (1) for regression in a)
+regInvA <- lm(X ~ Y1, data = random)
+summary(regInvA)
+# (2) for regression in b)
+regInvB <- lm(X ~ Y2, data = random)
+summary(regInvB)
