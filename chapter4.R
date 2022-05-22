374e7723640b723027ba283a22d6fef5ae5aa7f8
asluchych <anatolblr@gmail.com>
2022-05-22 17:58:39 +0200

    Update chapter4.R

--- chapter4.R
+++ chapter4.R
@@ -1,25 +1,25 @@
 ################################################################################
 #                                                                              #
-#      ‹bungsblatt 4: Vorhersagen, Bestimmtheitsmaﬂ und Modellierung           # 
-#                           Aufgabe 3                                          #
+#      Exercise 4: Prediction, Goodness-of-Fit and Modelling Issues            # 
+#                           Assignment 3                                       #
 #                                                                              #
 ################################################################################
 # package: if its not installed, type install.packages("tseries") in the console
 library(tseries) # for the jarque-bera-test function
 
 
-##### Aufgabe 3: H‰userpreise #####
+##### Assigment 3: House Prices #####
 
-# Daten einlesen
+# Read data
 houses <- read.csv("houseprices.csv")
 
-# a) Interpretation b_2 und R^2
-# Regressionen
-# Modell (1)
+# a) Interpretation b_2 and R^2
+# Regressions
+# Model (1)
 lin <- lm(PRICE ~ SQM, data = houses)
 summary(lin)
 
-# Modell (2)
+# Model (2)
 logLin <- lm(log(PRICE) ~ SQM, data = houses)
 summary(logLin)
 
@@ -27,12 +27,12 @@ summary(logLin)
 logLog <- lm(log(PRICE) ~ log(SQM), data = houses)
 summary(logLog)
 
-# b) Vergleich
+# b) Comparison 
 xVals <- seq(20, 800, 0.1)
 fittedValsLogLin <- exp(logLin$coefficients[1] + logLin$coefficients[2]*xVals)
 fittedValsLogLog <- exp(logLog$coefficients[1] + logLog$coefficients[2]*log(xVals))
 
-# (i) Graph der Regression - Scatterplot PRICE, SQM
+# (i) Regression graph - scatterplot PRICE, SQM
 plot(PRICE ~ SQM, data = houses, main = "Prices vs. Square Meters")
 abline(lin, col = "green")
 lines(fittedValsLogLin ~ xVals, col = "red")
@@ -40,16 +40,16 @@ lines(fittedValsLogLog ~ xVals, col = "blue")
 legend("topleft", legend = c("Linear", "Log-Linear", "Log-Log"),
        col = c("green", "red", "blue"), lwd = 1)
 
-# (ii) Residuen Graph
-# Linear Model
+# (ii) Residuals graph
+# Linear model
 plot(lin$residuals ~ houses$SQM, main = "Residuals Linear Model")
 abline(h=0)
 
-# Log-Linear Model
+# Log-linear model
 plot(logLin$residuals ~ houses$SQM, main = "Residuals Log-Linear Model")
 abline(h=0)
 
-# Log-Log Model
+# Log-log model
 plot(logLog$residuals ~ houses$SQM, main = "Residuals Log-Log Model")
 abline(h=0)
 
